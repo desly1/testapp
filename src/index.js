3a8a51e7656e6f80a7f97ff89a6169e7956e0f60
KACPER-PC\KACPER <kacper.pietrzyk@gmail.com>
2018-09-26 19:35:47 +0200

    Add param2 input with handler

--- src/index.js
+++ src/index.js
@@ -8,15 +8,20 @@ import Sample from './js/sample.js';
 // document.querySelector('#param1').onchange=handleUpdateParameter;
 
 const param1Element = document.querySelector('[data-myselector="param1"]'); // pobiram i zapisuje do zmiennej element z id param1
+const param2Element = document.querySelector ('#param2');
 const resultElement = document.querySelector('#result');
 
 param1Element.onchange=handleUpdateParam1; // dodawanie handlera (funkcji) obslugujaca zmiane wartosci w inpucie
+param2Element.onchange=handleUpdateParam2;
 
 document.querySelector('#action-add').onclick=sum; // dodawanie handlera na zmiane wartosci
 
 function handleUpdateParam1(ev) { //ev obiekt event ( w tym przypadku onchange ale moze byc click i wiele innych) dodawany przez przegladarke
     console.log(ev);
     console.log(ev.target.value); // pobieranie wartosci z inputa
+    resultElement.value=ev.target.value
+};
+function handleUpdateParam2(ev) {
     resultElement.value=ev.target.value
 };
 
