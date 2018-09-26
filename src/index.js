f79e172fc12c92b924bde2ac9a08e6fbf5b6506b
KACPER-PC\KACPER <kacper.pietrzyk@gmail.com>
2018-09-26 19:27:13 +0200

    Add handle parameter change,

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import './assets/styles/main.scss';
 import Sample from './js/sample.js';
 
+// querySelector - funkcja do pobierania pojedynczego elementu html
+// querySelector() - wywolanie funkcji
+// document.querySelector('[data-myselector="param1"]').onchange=handleUpdateParameter; // [data-myselector="param1"] - selector data atrybutu myselector
+// document.querySelector('.form-element').onchange=handleUpdateParameter;  // .form-element - selector po class elementu html 
+// document.querySelector('#param1').onchange=handleUpdateParameter;
 
-document.getElementById('app').innerHTML = Sample.say
+const param1Element = document.querySelector('[data-myselector="param1"]'); // pobiram i zapisuje do zmiennej element z id param1
+const resultElement = document.querySelector('#result');
+
+param1Element.onchange=handleUpdateParam1; // dodawanie handlera (funkcji) obslugujaca zmiane wartosci w inpucie
+
+document.querySelector('#action-add').onclick=sum; // dodawanie handlera na zmiane wartosci
+
+function handleUpdateParam1(ev) { //ev obiekt event ( w tym przypadku onchange ale moze byc click i wiele innych) dodawany przez przegladarke
+    console.log(ev);
+    console.log(ev.target.value); // pobieranie wartosci z inputa
+    resultElement.value=ev.target.value
+};
+
+function sum(ev) {
+    
+}
 
 
-document.getElementById('formula').value = 999;
-document.querySelector('#formula').value = 9999;
-document.querySelector('#formula').value = 9999;
-document.querySelector('#formula').value = 9999;
-document.querySelector('#formula').value = 9999;
 
-document.querySelector('.form-element').value = 1000;
+
+// gdzies tam w przegladarce handler (funkcja) jest wywolywana przez skrypt
+// kroki jak postepuje przegladarka:
+// 1. hej uzytkownik zmienial wartosc inputa
+// 2. uzytkownik przed kliknieciem podal mi funkcje ktora chce wywolac na zmiane wartosci
+// 3. wiec muszxe wywolac ta funkcje z parametrami ktore sa w standardzie js
+
+
